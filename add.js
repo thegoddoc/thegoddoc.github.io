747ad66a644178626fb8c35b3b3769e6957a72d9
thegoddoc <94640028+thegoddoc@users.noreply.github.com>
2021-12-01 08:08:38 +0200

    Create add.js

---
+++ add.js
@@ -0,0 +1,7 @@
+var body = document.body,
+    head = document.getElementById('head'),
+    topAdd = document.getElementById('add_top'),
+    h1 = document.createElement('h1');
+h1.innerHTML='H123456';
+
+topAdd.appendChild(h1);
