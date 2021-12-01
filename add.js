4fea7f192aaac91ee647c62b6a6ac3595bdb35c9
thegoddoc <94640028+thegoddoc@users.noreply.github.com>
2021-12-01 08:10:51 +0200

    Update add.js

--- add.js
+++ add.js
@@ -2,6 +2,6 @@ var body = document.body,
     head = document.getElementById('head'),
     topAdd = document.getElementById('add_top'),
     h1 = document.createElement('h1');
-h1.innerHTML='H123456';
+h1.innerHTML='H123456-22222222';
 
 topAdd.appendChild(h1);
